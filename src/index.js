c23ada71930d09f3065c8f89d82ae874c828786f
vidimamisokuhle <vidimamisokuhle@gmail.com>
2025-04-21 23:01:02 +0200

    updated changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayPoem(response) {
+function displayQuote(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -7,22 +7,22 @@ function displayPoem(response) {
   });
 }
 
-function generatePoem(event) {
+function generateQuote(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "ab3defb06170c8ob2cccb493b58t53ba";
   let context =
-    "You are a quotes expert and love to generate quotes of all quote authors around the world. Your mission is to generate a 4 line quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    "You are a quotes expert and love to generate quotes of all quote authors around the world. Your mission is to generate a quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote. Sign the quote with 'SheCodes AI' inside a <strong> element at the end of the quote and NOT at the beginning";
+  let prompt = `User instructions: Generate a quote in english about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
+  let poemElement = document.querySelector("#quote");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a quote poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let poemFormElement = document.querySelector("#quote-generator-form");
+poemFormElement.addEventListener("submit", generateQuote);
