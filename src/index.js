751ec709d2c752db49a7f409b5fbdfe0edbbfe00
vidimamisokuhle <vidimamisokuhle@gmail.com>
2025-04-21 13:44:26 +0200

    added  javascript functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Don't have dreams, chase them!",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
